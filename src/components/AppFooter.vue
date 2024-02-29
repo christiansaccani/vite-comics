02a2a72babe14ab3307f1975cd9bc558edd2b1bf
christiansaccani <christian.saccani99@gmail.com>
2024-02-29 19:22:09 +0100

    Exercise Done (with bonus)!

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,8 +4,7 @@ export default {
 
     data() {
         return {
-            socials: [
-            ],
+            socials: ["/img/footer-facebook.png", "/img/footer-twitter.png", "public/img/footer-youtube.png", "public/img/footer-pinterest.png", "/img/footer-periscope.png"],
         }
     }
 }
@@ -13,11 +12,75 @@ export default {
 
 <template>
     
+    <footer>
+        <div>
+            <a href="#">SIGN-UP NOW</a>
+            <div id="socials-container">
+                <p>FOLLOW US</p>
+                <ul id="socials-line">
+                    <li v-for="currentSocial in socials">
+                        <img :src=currentSocial>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
 </template>
 
 <style lang="scss">
     @use '../style/variables' as *;
     @use '../style/mixins' as *;
 
-    
+    footer {
+        @include centered;
+
+        height: calc(100vh - 370px - 130px - 155px - 120px);
+        width: 100%;
+
+        background-color: #171617;
+
+        z-index: 2;
+
+        div {
+            max-width: $maxWidth;
+            width: 100%;
+            height: 100%;
+
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            
+            a {
+                padding: 1em;
+                border: 2px solid $primaryColor;
+
+                width: 160px;
+
+                text-decoration: none;
+                color: white;
+            }
+
+            #socials-container {
+                display: flex;
+                justify-content: flex-end;
+                gap: 1.5em;
+
+                p {
+                    font-size: larger;
+                    font-weight: 700;
+                    color: $primaryColor
+                }
+
+                #socials-line {
+                    display: flex;
+                    gap: 1.5em;
+
+                    li {
+                        list-style: none; 
+                    }
+                }
+            }
+        }
+    }
 </style>
