267951fa3adf85748ebb4b83c5e3118461347d19
christiansaccani <christian.saccani99@gmail.com>
2024-03-01 13:38:39 +0100

    item section fully created

---
+++ src/components/ComicItem.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'ComicItem',
+
+    props: {
+        comicName: String,
+        comicImage: String
+    },
+
+    data() {
+    },
+}
+</script>
+
+<template>
+
+    <div class="comic">
+        <img :src="comicImage" :alt="comicName">
+        <p>{{ comicName }}</p>
+    </div>
+
+</template>
+
+<style lang="scss">
+
+.comic {
+    max-width: 190px !important;
+
+    img {
+        height: 190px !important;
+        object-fit: contain;
+    }
+
+    p{
+        font-size: small;
+        text-transform: uppercase;
+    }
+}
+
+</style>
